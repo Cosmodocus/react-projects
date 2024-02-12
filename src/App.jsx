b512595a1eb096985c6f173a43a6b80ec8a0cf67
Reza <149424191+Cosmodocus@users.noreply.github.com>
2024-02-12 15:21:48 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Accordian from './components/accordian';
 import ImageSlider from './components/image-slider';
+import LightDarkMode from './components/light-dark-mode';
 import LoadMoreData from './components/load-more-data';
+import QRCodeGenerator from './components/qr-code-generator';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import TreeView from './components/tree-view';
+import menus from './components/tree-view/data';
 
 function App() {
 	return (
@@ -18,13 +22,22 @@ function App() {
 
 			{/* Image Slider Component */}
 			{/* <ImageSlider
-        url={'https://picsum.photos/v2/list'}
-        limit={'20'}
-        page={'1'}
-      /> */}
+       			url={'https://picsum.photos/v2/list'}
+				limit={'20'}
+				page={'1'}
+      		/> */}
 
 			{/* Load More Button Component */}
 			{/* <LoadMoreData /> */}
+
+			{/* Tree View */}
+			<TreeView menus={menus} />
+
+			{/* QR Code Generator */}
+			{/* <QRCodeGenerator /> */}
+
+			{/* Light & Dark Mode Switch */}
+			{/* <LightDarkMode /> */}
 		</>
 	);
 }
